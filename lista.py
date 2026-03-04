dec0ed12691c79eb083473c2c553e3408502410b
Nicolaas dos Santos <nicolaasdossantos123@gmail.com>
2026-03-04 17:37:19 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,10 @@
+numeros = [5, 12, 7, 20, 3, 18, 1]
+resultado1 = numeros[:3]
+resultado2 = numeros[-2:]
+resultado3 = numeros[1:-1]
+resultado4 = numeros[::-1]
+
+print("3 Primeiros números", resultado1)
+print("2 Ultimos números", resultado2)
+print("Pula o primeiro e o ultimo número", resultado3)
+print("Reverter os números",resultado4)
